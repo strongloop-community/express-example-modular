46623c70457a54a102981188b221af2e8944e10e
Marc Harter <wavded@gmail.com>
2014-10-14 20:49:58 -0500

    add; more app structure

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 /* main module entry point */
-require('_/app')
+var cfg = require('_/config')
+var log = require('_/log')
+var app = require('_/app')
+
+app.listen(cfg.port)
+log.info('app listening on port', cfg.port)
