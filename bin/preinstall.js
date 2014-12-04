58f496b9aeb6edb2cc9040696a6215909f42bdba
Marc Harter <wavded@gmail.com>
2014-12-04 14:17:16 -0600

    fix; preinstall script to use preferred child proc

--- bin/preinstall.js
+++ bin/preinstall.js
@@ -1,14 +1,18 @@
 var fs = require('fs')
-var npm = require('npm')
-var path = require('path')
+var resolve = require('path').resolve
+var join = require('path').join
+var cp = require('child_process')
 
-var libDir = path.resolve(__dirname,'../lib/')
-var noop = function(){}
+// get library path
+var lib = resolve(__dirname, '../lib/')
 
-npm.load(function () {
-  fs.readdirSync(libDir).forEach(function (mod) {
-    npm.config.set('save', false) // https://github.com/npm/npm/issues/6391
-    npm.prefix = path.join(libDir, mod)
-    npm.commands.install(noop)
+fs.readdirSync(lib)
+  .forEach(function (mod) {
+    var modPath = join(lib, mod)
+
+    // ensure path has package.json
+    if (!fs.existsSync(join(modPath, 'package.json'))) return
+
+    // install folder
+    cp.spawn('npm', ['i'], { env: process.env, cwd: modPath, stdio: 'inherit' })
   })
-})
